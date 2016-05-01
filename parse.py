a4632af433d71d15ec4a4ec39430e8ac22dd423b
ajzwu8 <ajw238@cornell.edu>
2016-05-01 15:20:26 -0400

    parsed data into tables

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ files = ['sweep10', 'sweep11', 'sweep6', 'sweep7',
 'sweep8', 'sweep9']
 
 for filename in files:
-    with open(filename+".csv", 'rb') as f_in, open(filename+"new.csv", 'w') as f_out:
+    with open(filename+".csv", 'rb') as f_in, open(filename+"new.csv", 'wb') as f_out:
         csvreader = csv.reader(f_in)
         csvwriter = csv.writer(f_out)
         for row in csvreader:
---
+++ parse2.py
@@ -0,0 +1,30 @@
+import numpy as np
+import csv
+
+files = ['sweep6', 'sweep7',
+'sweep8', 'sweep9']
+
+col = 1
+
+for filen in files:
+    filename = filen + "new"
+    with open(filename+"table.csv", 'wb') as f_out:
+        csvwriter = csv.writer(f_out)
+        csvwriter.writerow(["Run","manSellers","Buyers","manBuyers","Sellers","riskAversion","maxRounds",
+                            "Trades","manTarget","actualPrice","finalPrice"])
+        while col < (640*4):
+            with open(filename+".csv", 'rb') as f_in: 
+                csvreader = csv.reader(f_in)
+                stuff = []
+                for row in csvreader:
+                    if csvreader.line_num <= 7 :
+                        stuff.append(row[col])
+                    if csvreader.line_num == 11:
+                        stuff.append(row[col])
+                        stuff.append(row[col+1])
+                        stuff.append(row[col+2])
+                        stuff.append(row[col+3])
+                # print stuff
+                csvwriter.writerow(stuff)
+                col += 4
+
