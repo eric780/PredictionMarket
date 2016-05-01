533a12b5dd2dcdea27020154a0b8e7f33f7a8d43
Michael Kuan <mmk248@cornell.edu>
2016-05-01 16:53:27 -0400

    lin reg

---
+++ organized/linreg.py
@@ -0,0 +1,27 @@
+from scipy import stats
+import numpy as np
+import csv
+
+files = ['manBoth', 'manBuyer', 'manNone','manSeller']
+
+for filen in files:
+	filename = filen + "LinReg" + ".csv"
+	with open (filename, 'wb') as f_out, open (filen+".csv", 'rb') as f_in:
+		csvwriter = csv.writer(f_out)
+		csvreader = csv.reader(f_in)
+		final = []
+		actual = []
+		target = []
+		for row in csvreader:
+			if csvreader.line_num != 1:
+				final.append(row[10])
+				actual.append(row[9])
+				target.append(row[8])
+		fin = np.array(map(float,final))
+		act = np.array(map(float,actual))
+		tar = np.array(map(float,target))
+		x = [abs(a - b) for a, b in zip(fin, act)]
+		y = [abs(a - b) for a, b in zip(fin, tar)]
+		
+		csvwriter.writerow(["slope","intercept","r-value","p_value","std_err"])
+		csvwriter.writerow(stats.linregress(x,y))
